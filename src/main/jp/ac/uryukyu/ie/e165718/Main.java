083ff666a0ea38e2ea0e708fc36c709dcd56e51c
Hiropon <e165718@ie.u-ryukyu.ac.jp>
2016-12-13 01:35:29 +0900

    NullPointException

--- src/main/jp/ac/uryukyu/ie/e165718/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165718/Main.java
@@ -2,13 +2,7 @@ package jp.ac.uryukyu.ie.e165718;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
+        String str = "3．14";
         double value = Double.parseDouble(str);
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerExceptionが起きた");
-            e.printStackTrace();
-        }
     }
 }
